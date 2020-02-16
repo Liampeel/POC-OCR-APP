3b3e08374cdea40926abddd43de9bae278189c44
dominic heyhoe <sc16dah@leeds.ac.uk>
2020-02-16 19:57:36 +0000

    Added a button that opens the camera

--- mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayFeaturesActivity.java
+++ mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/DisplayFeaturesActivity.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 import static com.example.myfirstapp.MainActivity.setWindowFlag;
@@ -19,30 +21,34 @@ public class DisplayFeaturesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_features);
 
-        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21)
-        {
+        if (Build.VERSION.SDK_INT >= 19 && Build.VERSION.SDK_INT < 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, true);
         }
-        if (Build.VERSION.SDK_INT >= 19)
-        {
+        if (Build.VERSION.SDK_INT >= 19) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
         //make fully Android Transparent Status bar
-        if (Build.VERSION.SDK_INT >= 21)
-        {
+        if (Build.VERSION.SDK_INT >= 21) {
             setWindowFlag(this, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false);
             getWindow().setStatusBarColor(Color.TRANSPARENT);
         }
 
-        ImageButton btn = (ImageButton)findViewById(R.id.homeButton);
+        ImageButton btn = (ImageButton) findViewById(R.id.homeButton);
+        Button cambtn = (Button) findViewById(R.id.button);
 
-        btn.setOnClickListener(new View.OnClickListener()
-        {
+        btn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 startActivity(new Intent(DisplayFeaturesActivity.this, DisplayFeaturesActivity.class));
             }
         });
+
+        cambtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+            });
     }
 }
