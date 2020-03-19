7caa7f56e0b2518ad63502cf24be3ee79c10021d
dominic heyhoe <sc16dah@leeds.ac.uk>
2020-03-19 17:59:04 +0000

    Added more validation to app - login page this time

--- mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -7,10 +7,13 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "Email Address";
@@ -49,21 +52,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    // Called when the user taps the send button
-    public void sendMessage(View view)
+    public void login(View view)
     {
-        Intent intent = new Intent(this, DisplayFeaturesActivity.class);
-
         EditText emailText = findViewById(R.id.emailText);
         EditText passwordText = findViewById(R.id.passwordText);
 
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
 
-        intent.putExtra(EXTRA_MESSAGE, email);
-        intent.putExtra(EXTRA_MESSAGE2, password);
-
-        startActivity(intent);
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(MainActivity.this,"No email was entered, please enter an email to continue",Toast.LENGTH_LONG).show();
+        }
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            Toast.makeText(MainActivity.this,"The email address entered is not valid",Toast.LENGTH_LONG).show();
+        }
+        else if(TextUtils.isEmpty(password)){
+            Toast.makeText(MainActivity.this,"No password was entered, please enter an password to continue",Toast.LENGTH_LONG).show();
+        }
+        else {
+            Intent intent = new Intent(this, DisplayFeaturesActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, email);
+            intent.putExtra(EXTRA_MESSAGE2, password);
+            startActivity(intent);
+        }
     }
 
     public void registerPage(View view)
