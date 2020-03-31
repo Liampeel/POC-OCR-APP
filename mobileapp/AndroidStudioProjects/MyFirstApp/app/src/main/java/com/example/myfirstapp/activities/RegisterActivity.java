70904193600d6aa51b11353320f01b7e39794df9
Dominic Heyhoe <sc16dah@leeds.ac.uk>
2020-03-31 14:32:11 +0100

    Made some changes to the register activity

--- mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/activities/RegisterActivity.java
+++ mobileapp/AndroidStudioProjects/MyFirstApp/app/src/main/java/com/example/myfirstapp/activities/RegisterActivity.java
@@ -12,7 +12,6 @@ import android.graphics.Color;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
 
 import com.example.myfirstapp.API.RetrofitClient;
 import com.example.myfirstapp.Model.DefaultResponse;
@@ -28,8 +27,6 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-
-import com.example.myfirstapp.activities.MainActivity;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -106,151 +103,121 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         win.setAttributes(winParams);
     }
 
-//    public void LoginDirect(View view)
-//    {
-//        Intent intent = new Intent(this, MainActivity.class);
-//        startActivity(intent);
-//    }
-//
-//    public void Register(View view)
-//    {
-//        EditText emailText = findViewById(R.id.emailText);
-//        EditText passwordText = findViewById(R.id.passwordText);
-//        EditText nameText = findViewById(R.id.nameText);
-//
-//        String email = emailText.getText().toString();
-//        String password = passwordText.getText().toString();
-//        String name = nameText.getText().toString();
-//
-//        Intent checkIntent = new Intent(getApplicationContext(), RegisterActivity.class);
-//
-//        if (TextUtils.isEmpty(email)) {
-//            Toast.makeText(RegisterActivity.this,"No email was entered, please enter an email to continue",Toast.LENGTH_LONG).show();
-//        }
-//        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
-//            Toast.makeText(RegisterActivity.this,"The email address entered is not valid",Toast.LENGTH_LONG).show();
-//        }
-//        else if(TextUtils.isEmpty(password)){
-//            Toast.makeText(RegisterActivity.this,"No password was entered, please enter an password to continue",Toast.LENGTH_LONG).show();
-//        }
-//        else if (TextUtils.isEmpty(name)){
-//            startActivity(checkIntent);
-//            Toast.makeText(RegisterActivity.this,"No name was entered, please enter an name to continue",Toast.LENGTH_LONG).show();
-//        }
-//        else {
-//            Resources res = getResources();
-//            String[] terms = res.getStringArray(R.array.terms_and_conditions);
-//            AlertDialog.Builder termsDialogBuilder = new AlertDialog.Builder(this);
-//            termsDialogBuilder.setTitle("Terms and Conditions");
-//            termsDialogBuilder.setMessage(terms[0] + terms[1] + terms[2] + terms[3] + terms[4] + terms[5] + terms[6]);
-//            termsDialogBuilder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface arg0, int arg1) {
-//                    Toast.makeText(RegisterActivity.this, "Thank you for Registering", Toast.LENGTH_LONG).show();
-//                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-//
-//                    Spinner yearSpinItem = findViewById(R.id.yearspin);
-//                    Spinner monthSpinItem = findViewById(R.id.monthspin);
-//                    Spinner daySpinItem = findViewById(R.id.dayspin);
-//
-//                    Integer day = Integer.parseInt((String) daySpinItem.getSelectedItem());
-//                    String month = monthSpinItem.getSelectedItem().toString();
-//                    Integer year = Integer.parseInt((String) yearSpinItem.getSelectedItem());
-//
-//                    String dateOfBirth = year + "/" + month + "/" + day;
-//
-//                    startActivity(intent);
-//                }
-//            });
-//
-//            termsDialogBuilder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
-//                @Override
-//                public void onClick(DialogInterface dialog, int which) {
-//                    Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
-//                    finish();
-//                    startActivity(intent);
-//                    Toast.makeText(RegisterActivity.this, "Please Accept the Terms and Conditions", Toast.LENGTH_LONG).show();
-//                }
-//            });
-//
-//            AlertDialog alertDialog = termsDialogBuilder.create();
-//            alertDialog.show();
-//
-//        }
-//
-//    }
 
     private void userSignUp() {
         String email = editTextEmail.getText().toString().trim();
         String password = editTextPassword.getText().toString().trim();
         String name = editTextName.getText().toString().trim();
 
+        System.out.println("Made it to the user sign up method");
+
         if (email.isEmpty()) {
             editTextEmail.setError("Email is required");
             editTextEmail.requestFocus();
             return;
         }
-
-        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             editTextEmail.setError("Enter a valid email");
             editTextEmail.requestFocus();
             return;
         }
-
-        if (password.isEmpty()) {
+        else if (password.isEmpty()) {
             editTextPassword.setError("Password is required");
             editTextPassword.requestFocus();
             return;
         }
-
-        if (name.isEmpty()) {
+        else if (name.isEmpty()) {
             editTextName.setError("Name is required");
             editTextName.requestFocus();
             return;
         }
-
-        if (password.length() < 2) {
-            editTextPassword.setError("Password must be atleast 2 characters");
+        else if (password.length() < 2) {
+            editTextPassword.setError("Password must be at least 2 characters");
             editTextPassword.requestFocus();
             return;
         }
+        else {
+            System.out.println("Made it to the else statement, the bulk of the call");
+            Call<DefaultResponse> call = RetrofitClient
+                    .getInstance()
+                    .getApi()
+                    .createUser(email, password, name);
 
-        Call<DefaultResponse> call = RetrofitClient
-                .getInstance()
-                .getApi()
-                .createUser(email, password, name);
-
-        call.enqueue(new Callback<DefaultResponse>() {
-            @Override
-            public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
-                if(response.code() == 201){
-                    DefaultResponse dr = response.body();
-                    Toast.makeText(RegisterActivity.this, dr.getEmailMsg(), Toast.LENGTH_SHORT).show();
-                }else{
-
-                    Toast.makeText(RegisterActivity.this, "User already Exists", Toast.LENGTH_SHORT).show();
+            call.enqueue(new Callback<DefaultResponse>() {
+                @Override
+                public void onResponse(Call<DefaultResponse> call, Response<DefaultResponse> response) {
+                    System.out.println("Made if to the response code if statement");
+                    if (response.code() == 200) {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        DefaultResponse dr = response.body();
+                        Toast.makeText(RegisterActivity.this, dr.getEmailMsg(), Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(RegisterActivity.this, "Thank you for Registering", Toast.LENGTH_LONG).show();
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(RegisterActivity.this, "User already Exists", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
+                @Override
+                public void onFailure(Call<DefaultResponse> call, Throwable t) {
+                    Log.d("Register", t.getMessage());
+                }
+            });
 
+        }
+    }
+
+    public void register()
+    {
+        Resources res = getResources();
+        String[] terms = res.getStringArray(R.array.terms_and_conditions);
+        AlertDialog.Builder termsDialogBuilder = new AlertDialog.Builder(this);
+        termsDialogBuilder.setTitle("Terms and Conditions");
+        termsDialogBuilder.setMessage(terms[0] + terms[1] + terms[2] + terms[3] + terms[4] + terms[5] + terms[6]);
+        termsDialogBuilder.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
             @Override
-            public void onFailure(Call<DefaultResponse> call, Throwable t) {
-                Log.d("Register", t.getMessage());
+            public void onClick(DialogInterface arg0, int arg1) {
+                //Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                Spinner yearSpinItem = findViewById(R.id.yearspin);
+                Spinner monthSpinItem = findViewById(R.id.monthspin);
+                Spinner daySpinItem = findViewById(R.id.dayspin);
+
+                Integer day = Integer.parseInt((String) daySpinItem.getSelectedItem());
+                String month = monthSpinItem.getSelectedItem().toString();
+                Integer year = Integer.parseInt((String) yearSpinItem.getSelectedItem());
+
+                String dateOfBirth = year + "/" + month + "/" + day;
+
+                userSignUp();
+
+                //startActivity(intent);
             }
         });
 
-    }
+        termsDialogBuilder.setNegativeButton("Decline", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                finish();
+                startActivity(intent);
+                Toast.makeText(RegisterActivity.this, "Please Accept the Terms and Conditions", Toast.LENGTH_LONG).show();
+            }
+        });
 
+        AlertDialog alertDialog = termsDialogBuilder.create();
+        alertDialog.show();
+
+    }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.registerButton:
-                userSignUp();
-                startActivity(new Intent(this, DisplayFeaturesActivity.class));
+                register();
                 break;
             case R.id.loginButton:
                 startActivity(new Intent(this, MainActivity.class));
-
                 break;
         }
     }
